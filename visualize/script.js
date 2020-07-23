4aab646d618fa712175108d4757566025c06358d
Leah Mamaril <leahmamaril@Leahs-MacBook-Pro.local>
2020-07-22 20:42:13 -0700

    added link to stylesheet

--- visualize/script.js
+++ visualize/script.js
@@ -15,3 +15,19 @@ svgSelection.append("circle")
 d3.selectAll("h1")
        .style("color", "#22232a")
        .style("font-family", "verdana")
+
+// Test add data
+var trailData = 
+[["Harding Ice Field Trail", "Kenai Fjords National Park", 1161.8976],
+["Grandview Point to Horseshoe Mesa Trail","Grand Canyon National Park", 726.948],
+["Clouds Rest Trail via Tenaya Lake", "Yosemite National Park", 948.8424]]
+
+// adding the elevation gain header
+d3.select("body").append("h4").text("Elevation Gain")
+
+d3.select("body").selectAll("p")
+       .data(trailData)
+       .enter()
+       .append("p")
+       .text(function(trail) {return trail[0] + " " + trail[2]})
+
