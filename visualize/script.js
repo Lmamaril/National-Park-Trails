04a49a7226aff717a6792fb656c6c5fd107b301d
Leah Mamaril <leahmamaril@Leahs-MacBook-Pro.local>
2020-07-23 05:16:24 -0700

    added graph, removed circle

--- visualize/script.js
+++ visualize/script.js
@@ -1,21 +1,3 @@
-// Test addition of a circle
-var bodySelection = d3.select("body");
- 
-var svgSelection = bodySelection.append("svg")
-       .attr("width", 50)
-       .attr("height", 50);
- 
-svgSelection.append("circle")
-    .attr("cx", 25)
-    .attr("cy", 25)
-    .attr("r", 25)
-    .style("fill", "#dc143c");
-
-// Test edit header
-d3.selectAll("h1")
-       .style("color", "#22232a")
-       .style("font-family", "verdana")
-
 // Test add data
 var trailData = 
 [["Harding Ice Field Trail", "Kenai Fjords National Park", 1161.8976],
@@ -30,4 +12,53 @@ d3.select("body").selectAll("p")
        .enter()
        .append("p")
        .text(function(trail) {return trail[0] + " " + trail[2]})
+
+// Horizontal bar graph attempt
+var margin = {
+       top: 20,
+       right: 30,
+       bottom: 30,
+       left: 220
+};
+
+var width = 600 - margin.left - margin.right;
+var height = 460 - margin.top - margin.bottom;
+
+var svg = d3.select("#elevationGraph")
+       .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+       .append("g")
+              .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+// create x axis
+var x = d3.scaleLinear()
+       .domain([0, 1600])
+       .range([ 0, width]);
+svg.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(d3.axisBottom(x))
+       .selectAll("text")
+              .attr("transform", "translate(-10,0)rotate(-45)")
+              .style("text-anchor", "end");
+
+// create y axis
+var y = d3.scaleBand()
+       .range([ 0, height ])
+       .domain(trailData.map(function(d) { return d[0]; }))
+       .padding(.1);
+svg.append("g")
+       .call(d3.axisLeft(y))
+
+// add bars
+svg.selectAll("trailRect")
+.data(trailData)
+.enter()
+.append("rect")
+.attr("x", x(0) )
+.attr("y", function(trail) { return y(trail[0]); })
+.attr("width", function(trail) { return x(trail[2]); })
+.attr("height", y.bandwidth() )
+.attr("fill", "#4c9a2a")
 
